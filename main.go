3c14ffda6e841acfb217eacd2c2933d00728e5fa
nahima97 <nahima.sultana@gmail.com>
2025-11-20 18:41:50 +0000

    server added

--- main.go
+++ main.go
@@ -1,2 +1,40 @@
 package main
 
+import (
+	"fmt"
+	"vigilant-spork/db"
+	"vigilant-spork/handlers"
+	"vigilant-spork/repository"
+	"vigilant-spork/routes"
+	"vigilant-spork/services"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	Db := db.InitDb()
+
+	userRepo := &repository.UserRepo{Db: Db}
+	productRepo := &repository.ProductRepo{Db: Db}
+	cartRepo := &repository.CartRepo{Db: Db}
+	reviewRepo := &repository.ReviewRepo{Db: Db}
+
+	userService := &services.UserService{UserRepo: userRepo}
+	productService := &services.ProductService{ProductRepo: productRepo}
+	cartService := &services.CartService{CartRepo: cartRepo}
+	reviewService := &services.ReviewService{ReviewRepo: reviewRepo}
+
+	userHandler := &handlers.UserHandler{Service: userService}
+	productHandler := &handlers.productHandler{Service: productService}
+	cartHandler := &handlers.cartHandler{Service: cartService}
+	reviewHandler := &handlers.reviewHandler{Service: reviewService}
+
+	r := routes.SetupRouter(userHandler, productHandler, cartHandler, reviewHandler)
+
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {
+		log.Fatal("failed to start server", err)
+	}
+	fmt.Println("server started!")
+}
